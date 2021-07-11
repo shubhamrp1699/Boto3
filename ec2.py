54573655649b29cba6f3eeb138ab5f78e268a350
Shubham Pandit <shubhamrp1699@gmail.com>
2021-07-11 10:14:56 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,14 +1,9 @@
-# Lets create EC2 instances using Python BOTO3
+# Lets create/describe/terminate/stop/reboot EC2 instances using Python BOTO3
 import boto3
 
 
 def create_ec2_instance():
-    """
-    MaxCount=1, # Keep the max count to 1, unless you have a requirement to increase it
-    InstanceType="t2.micro", # Change it as per your need, But use the Free tier one
-    KeyName="ec2-key" # Change it to the name of the key you have.
-    :return: Creates the EC2 instance.
-    """
+
     try:
         print ("Creating EC2 instance")
         resource_ec2 = boto3.client("ec2")
